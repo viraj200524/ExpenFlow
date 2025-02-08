821b65c8f2f5c4c80848e154bf4d0a395b357417
chaitra-samant <chaitrasamant27@gmail.com>
2025-02-08 11:18:49 +0530

    Initial Setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='flex justify-center items-center h-screen'>
+        <h1 className='text-5xl'>Hello World</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
